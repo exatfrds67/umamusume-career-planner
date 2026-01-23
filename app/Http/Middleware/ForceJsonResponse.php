82caf69c8d628e7ffea737c9542b29357ccf6719
IzzatFirdaus <2021601074@student.uitm.edu.my>
2026-01-23 20:48:54 +0800

    feat(http): add security headers and JSON response middleware

---
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // For POST, PUT, PATCH requests, enforce JSON content type
+        if (in_array($request->method(), ['POST', 'PUT', 'PATCH']) && ! $request->isJson()) {
+            return response()->json([
+                'message' => 'Content-Type must be application/json',
+            ], 415);
+        }
+
+        $request->headers->set('Accept', 'application/json');
+
+        return $next($request);
+    }
+}
